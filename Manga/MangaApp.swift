023cae3746e8163e1ab7f196ebfdc95f2592fccf
Alvin Askoolum <plaids-midsize0n@icloud.com>
2024-11-26 09:43:04 +0400

    Initial Commit

---
+++ Manga/MangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaApp.swift
+//  Manga
+//
+//  Created by Alvin Askoolum on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
